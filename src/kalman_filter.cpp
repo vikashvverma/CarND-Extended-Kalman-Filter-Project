9df646208c46ff656b2c7fd3205a75b750f19c4f
vikashvverma <vikash@programminggeek.in>
2019-01-14 11:45:18 +0000

    Complete project

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,84 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = PHt * Si;
+
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
+
+VectorXd CartesianToPolar(const VectorXd &x_state){
+  /*
+   * convert radar measurements from cartesian coordinates to polar coordinates
+  */
+  float px, py, vx, vy;
+  px = x_state[0];
+  py = x_state[1];
+  vx = x_state[2];
+  vy = x_state[3];
+
+  float rho, phi, rho_dot;
+  rho = sqrt(px*px + py*py);
+  phi = atan2(py, px);
+
+  if(rho < 0.000001)
+    rho = 0.000001;
+
+  rho_dot = (px * vx + py * vy) / rho;
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho, phi, rho_dot;
+
+  return z_pred;
+}
+
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  
+//   Cartesian to polar
+  VectorXd z_pred = CartesianToPolar(x_);
+  VectorXd y = z - z_pred;
+  
+  // normalize angles
+  while(y(1) > M_PI){
+    y(1) -= 2*M_PI;
+  }
+  
+  while(y(1) < -M_PI){
+    y(1) += 2*M_PI;
+  }
+  
+  // Update
+  MatrixXd Ht = H_.transpose();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = PHt * Si;
+  
+  
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
